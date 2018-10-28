93551440d366dfd337f1c0c70e580457d0019419
Nikki A <nikki.apostolakis@gmail.com>
2018-10-28 16:52:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,5 +24,10 @@ class ApplicationController < Sinatra::Base
     erb :new
   end
 
+  post 'recipes' do
+    @recipe = Recipe.create(:name =>params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
 
 end
